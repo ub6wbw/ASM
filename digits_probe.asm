7ce1a94aff55b61fcee77d43e110e0955e6f4a40
Magomed <98849651+ub6wbw@users.noreply.github.com>
2022-10-30 23:22:14 +0300

    Add files via upload

--- digits_probe.asm
+++ digits_probe.asm
@@ -6,12 +6,36 @@ section .data
 
 section .text 
 _start:
+	;al, bl, cl, dl := 0x00
+	xor al, al
+	xor bl, bl
+	xor cl, cl
+	xor dl, dl
+
+	;ah, bh, ch, dh := 0x00
+	xor ah, ah
+	xor bh, bh
+	xor ch, ch
+	xor dh, dh
+
+	;ax, bx, cx, dx := 0x00
+	xor ax, ax
+	xor bx, bx
+	xor cx, cx
+	xor dx, dx
+
+	;eax, ebx, ecx, edx := 0x00
+	xor eax, eax
+	xor ebx, ebx
+	xor ecx, ecx
+	xor edx, edx
+
 	;rax, rbx, rcx, rdx := 0x00
 	xor rax, rax
 	xor rbx, rbx
 	xor rcx, rcx
 	xor rdx, rdx
-	
+
 	;al := al + bl (-1 + -1)
 	mov al, -1
 	mov bl, -1
